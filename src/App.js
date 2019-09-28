db2913e04c138a9c7c3d7874ab5abd293702e105
Veljko Blagojevic <veljko@geta.no>
2019-09-28 19:16:43 +0200

    Adds css-in-js emotion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
-  const theme = useState("darkblue");
-  return (
-    <ThemeContext.Provider value={theme}>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    </ThemeContext.Provider>
-  );
+    const theme = useState("darkblue");
+    return (
+        <ThemeContext.Provider value={theme}>
+            <div>
+                <NavBar />
+                <Router>
+                    <SearchParams path="/" />
+                    <Details path="/details/:id" />
+                </Router>
+            </div>
+        </ThemeContext.Provider>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export default {
+    primary: "#bf3334",
+    secondary: "#d9c14f",
+    dark: "#122622",
+    light: "#81a69b"
+}

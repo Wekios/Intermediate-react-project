9ef09b8173ffa3e01d52e7e0d9a15c71e7d3ca6d
Veljko Blagojevic <veljko@geta.no>
2019-09-28 15:05:57 +0200

    restart of the project for intermediate react

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Link } from '@reach/router';
-import Details from './components/Details';
-import SearchParams from './components/SearchParams';
-import ThemeContext from './ThemeContext';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
-
-    const themeHook = useState('peru');
-
-    return (
-        <React.StrictMode>
-            <ThemeContext.Provider value={themeHook}>
-                <div>
-                    <header>
-                        <Link to="/">Adopt me!</Link>
-                    </header>
-                    <Router>
-                        <SearchParams path="/" />
-                        <Details path="details/:id" />
-                    </Router>
-                </div>
-            </ThemeContext.Provider>
-        </React.StrictMode>
-    )
+  const theme = useState("darkblue");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));

0b17a86a2b22350ae5639234c2526e5cf15ae16c
Veljko Blagojevic <veljko@geta.no>
2019-09-28 19:45:51 +0200

    Adds code splitting that parcel or webpack pick up auto

--- src/Details.js
+++ src/Details.js
@@ -5,6 +5,10 @@ import Carousel from "./Carousel";
 import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import _ from "lodash";
+import moment from "moment";
+
+console.log(_, moment);
 
 class Details extends React.Component {
     state = { loading: true, showModal: false };
@@ -16,7 +20,7 @@ class Details extends React.Component {
                     animal: animal.type,
                     location: `${animal.contact.address.city}, ${
                         animal.contact.address.state
-                    }`,
+                        }`,
                     description: animal.description,
                     media: animal.photos,
                     breed: animal.breeds.primary,
